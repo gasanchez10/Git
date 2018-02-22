4e3ec0f657622b20232a6b6a0c742f31f654c54b
BVCAD\pasante_innovacion <pasanteinnovacion@bvc.com.co>
2018-02-22 09:27:52 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Form,FormGroup,FormControl, ControlLabel, Button} from 'react-bootstrap'
+
+class App extends Component {
+  render() {
+    return (
+
+<div id="div">
+
+  <h1>SOY UNA FORMA DE PRUEBA MAIN BRANCH </h1>
+<form method="post" className="Hola">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+    </div>
+    );
+  }
+}
+
+export default App;
+
+
+
