5209cc6f2fed2bcaba6b8873a4d73e264661d43d
BVCAD\pasante_innovacion <pasanteinnovacion@bvc.com.co>
2018-02-22 09:34:01 -0500

    Dev Branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 
 <div id="div">
 
-  <h1>SOY UNA FORMA DE PRUEBA MAIN BRANCH </h1>
+  <h1>SOY UNA FORMA DE PRUEBA DEVELOPER BRANCH </h1>
 <form method="post" className="Hola">
           <input type="hidden" name="form-name" value="contact" />
           <p>
