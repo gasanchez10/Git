7bc924b955906d067a6110fa304de4883f3da545
BVCAD\pasante_innovacion <pasanteinnovacion@bvc.com.co>
2018-02-23 08:26:21 -0500

    Rama principal

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 
 <div id="div">
 
-  <h1>SOY UNA FORMA DE PRUEBA DEVELOPER BRANCH </h1>
+  <h1>SOY UNA FORMA DE PRUEBA RAMA PRINCIPAL </h1>
 <form method="post" className="Hola">
           <input type="hidden" name="form-name" value="contact" />
           <p>
